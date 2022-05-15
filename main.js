53c6b5558b0d8b516c361aa7de466c1365fb7d67
Tricia <67928545+GTcreyon@users.noreply.github.com>
2022-05-15 22:25:23 +0100

    split templates into separate file

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// this is just here to circumvent an issue with StandardJS while keeping the code clean
+const templates = exportTemplates // eslint-disable-line
+
 const tokenTypes = ['nf', 'ns', 'ps', 'po', 'pa', 'pp', 'pr', 'tc', 'tf', 'tn']
 const profileDefaults = {
   nf: 'Creyon',
@@ -52,7 +55,6 @@ function replaceTokens (input) {
       if (replacement === '') {
         replacement = profileDefaults[mainStr]
       }
-      console.log(token)
       replacement = replacement.toLowerCase()
       if (parts.length > 1) {
         const params = parts[1].split(',')
@@ -107,27 +109,3 @@ function grabProfile () {
   })
   return profile
 }
-
-// TEMPLATE CONTENT
-
-// key:
-// nf - forename, primary name
-// ps/ey - subject
-// po/em - object
-// pa/eir - possessive adj
-// pp/eirs - possessive pronoun
-// pr/emself - reflexive
-
-const templates = [
-  'Hey, where is %nf;c%? I can\'t find %po% anywhere! %ps;c% said %ps% $p;was,were$ going to get %pr% to the store today, but %pa% car is still on the driveway.',
-  '%nf;c% looks great in %pa% new outfit!',
-  '%nf;c% told me to meet at %pa% house.',
-  'Have you seen %nf;c%? %ps;c% said %ps% would be here.',
-  '%nf;c%? Yeah, I think I saw %po% out back.',
-  'I know this really cool %tc% called %nf;c%!!',
-  '%ps;c%\'$p;s,re$ super awesome and I\'m really happy to be around %po%.',
-  '%pa;c% personality is super unique, and I really love that!!',
-  '%ps;c% deserve$p;s,$ so much love, and I hope %ps% continue$p;s,$ to practice self care and love %pr% <3',
-  'Hey, look! It\'s %nf;c%!',
-  'I wonder what %ps%$p;\'s,\'re$ up to, and I hope %pa% day\'s going okay.'
-]
---
+++ templates.js
@@ -0,0 +1,23 @@
+// TEMPLATE CONTENT
+
+// key:
+// nf - forename, primary name
+// ps/ey - subject
+// po/em - object
+// pa/eir - possessive adj
+// pp/eirs - possessive pronoun
+// pr/emself - reflexive
+
+const exportTemplates = [ // eslint-disable-line
+  'Hey, where is %nf;c%? I can\'t find %po% anywhere! %ps;c% said %ps% $p;was,were$ going to get %pr% to the store today, but %pa% car is still on the driveway.',
+  '%nf;c% looks great in %pa% new outfit!',
+  '%nf;c% told me to meet at %pa% house.',
+  'Have you seen %nf;c%? %ps;c% said %ps% would be here.',
+  '%nf;c%? Yeah, I think I saw %po% out back.',
+  'I know this really cool %tc% called %nf;c%!!',
+  '%ps;c%\'$p;s,re$ super awesome and I\'m really happy to be around %po%.',
+  '%pa;c% personality is super unique, and I really love that!!',
+  '%ps;c% deserve$p;s,$ so much love, and I hope %ps% continue$p;s,$ to practice self care and love %pr% <3',
+  'Hey, look! It\'s %nf;c%!',
+  'I wonder what %ps%$p;\'s,\'re$ up to, and I hope %pa% day\'s going okay.'
+]
